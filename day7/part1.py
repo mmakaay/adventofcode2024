bc5e13a3c57e8f7243974c5639d358b5bd7ecaaa
Your Name <you@example.com>
2024-12-07 10:13:39 +0100

    Add day 7

---
+++ day7/part1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def read_equations():
+    for line in sys.stdin:
+        [test_value, numbers] = line.split(":")
+        yield int(test_value), list(map(int, numbers.split()))
+
+
+def is_possible_equation(test_value, numbers):
+    for result in recurse_equation(numbers[0], numbers[1:]):
+        if result == test_value:
+            return True
+    return False
+
+
+def recurse_equation(total, numbers):
+    if not numbers:
+        yield total
+        return
+
+    next_number = numbers[0]
+    remaining = numbers[1:]
+    yield from recurse_equation(total * next_number, remaining)
+    yield from recurse_equation(total + next_number, remaining)
+
+
+total = 0
+for test_value, numbers in read_equations():
+    if is_possible_equation(test_value, numbers):
+        total += test_value
+
+print("Calibration value:", total)
