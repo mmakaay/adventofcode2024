2d11918f9f54ef451ec6715252434da15da1342e
Your Name <you@example.com>
2024-12-10 17:54:11 +0100

    Add day 10

---
+++ day10/golf2.py
@@ -0,0 +1,8 @@
+import sys
+M=[list(map(int,l.strip()))for l in sys.stdin]
+W,H=len(M[0]),len(M)
+def S(x,y,T=0):
+ for X,Y in[(x-1,y),(x+1,y),(x,y-1),(x,y+1)]:
+  if 0<=X<W and 0<=Y<H and M[Y][X]==M[y][x]+1:T=S(X,Y,T)
+ return T+(M[y][x]>8)
+print(sum(S(x,y)for y in range(H)for x in range(W)if M[y][x]<1))
---
+++ day10/part2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def load_topography():
+    heights = [list(map(int, line.strip())) for line in sys.stdin]
+    return (len(heights[0]), len(heights), heights)
+
+
+def find_trailheads(topography):
+    w, h, heights = topography
+    return ((x, y) for y in range(h) for x in range(w) if heights[y][x] == 0)
+
+
+def hike_to_all_tops(topography, start):
+    w, h, heights = topography
+    x, y = start
+    current_height = heights[y][x]
+
+    if current_height == 9:
+        yield (x, y)
+        return
+
+    if x > 0 and heights[y][x - 1] == current_height + 1:
+        yield from hike_to_all_tops(topography, (x - 1, y))
+    if x < w - 1 and heights[y][x + 1] == current_height + 1:
+        yield from hike_to_all_tops(topography, (x + 1, y))
+    if y > 0 and heights[y - 1][x] == current_height + 1:
+        yield from hike_to_all_tops(topography, (x, y - 1))
+    if y < h - 1 and heights[y + 1][x] == current_height + 1:
+        yield from hike_to_all_tops(topography, (x, y + 1))
+
+
+total = 0
+
+topography = load_topography()
+for trailhead in find_trailheads(topography):
+    for route in hike_to_all_tops(topography, trailhead):
+        total += 1
+
+print("Total score:", total)
