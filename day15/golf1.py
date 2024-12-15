4d4fc3fe756587786d3f5e09c4756f0f61ae4baf
Your Name <you@example.com>
2024-12-15 23:25:42 +0100

    Add golf solution for day 15, part 1

---
+++ day15/golf1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+n,W,P,R=enumerate,[],"",0
+for y,l in n(sys.stdin):
+ if l[0]=="#":r=list(l);W+=[r];R=(r.index("@"),y)if"@"in r else R
+ else:P+=l.strip()
+for D in P:
+ b,P,(x,y),(v,w)="@",[],R,{"<":(-1,0),">":(1,0),"^":(0,-1),"v":(0,1)}[D];P+=[R]
+ while b in"@O":x+=v;y+=w;P+=[(x,y)];b=W[y][x];P*=b!="#"
+ for i,(x,y)in n(P):W[y][x]=".@O"[min(i,2)];R=[R,(x,y)][i==1]
+print(sum(x+100*y for y,r in n(W)for x,e in n(r)if e=="O"))
