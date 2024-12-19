23fc60052c81e479caf719ffc23deea2869dc593
Your Name <you@example.com>
2024-12-19 11:09:32 +0100

    Improve day 18 part 2 golf solution

--- 18_RAM_Run/golf2.py
+++ 18_RAM_Run/golf2.py
@@ -1,17 +1,16 @@
-from heapq import heappush as I, heappop as O
-def X(n):
- w,h=[1+max(v[i]for v in M)for i in[0, 1]]
- S,E,D,Q,C=(0,0),(w-1,h-1),[(0,-1),(1,0),(0,1),(-1,0)],[],{};C[S]=0;I(Q,(0,S))
+from heapq import*
+M=[tuple(map(int,l.split(',')))for l in open(0)]
+def f(n):
+ S=M[:n];w=max(a[0]for a in M)+1;h=max(a[1]for a in M)+1;Q=[(0,(0,0))];D,r,t={(0,0):0},0,-1
  while Q:
-  c,(x,y)=O(Q)
-  if (x,y)==E:return c
-  for f,g in D:
-   j,k=x+f,y+g;d=c+1;s=(j,k)
-   if 0<=j<w and 0<=k<h and not(j,k)in M[:n]and(d<C.get(s,w*h)):C[s]=d;I(Q,(d,s))
-M=[tuple(map(int,l.strip().split(",")))for l in open(0)];L,U=0,len(M);A=0
-while True:
- P=L+(U-L)//2
- if P==A:print(M[U-1]);break
- A=P
- if X(P):L=P
+  c,(x,y)=heappop(Q);i=5
+  if(x,y)==(w-1,h-1):return c
+  while i:=i-1:
+   r,t=-t,r;X,Y=x+r,y+t;d=c+1;s=(X,Y)
+   if 0<=X<w and 0<=Y<h and s not in S and d<D.get(s,w*h):D[s]=d;heappush(Q,(d,s))
+L,U,A,P=0,len(M),0,0
+while U!=A:
+ P=(L+U)//2;A=P
+ if f(P):L=P
  else:U=P
+print(M[U-1])
