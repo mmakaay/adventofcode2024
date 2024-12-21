99df57ea7b383599925bebfc57e983f4765a243b
Your Name <you@example.com>
2024-12-21 20:45:52 +0100

    Improve day 11, part 2 golf

--- 11_Plutonian_Pebbles/golf2.py
+++ 11_Plutonian_Pebbles/golf2.py
@@ -1,11 +1,11 @@
 from collections import Counter as C
-A=C(next(open(0)).split())
-for _ in range(75):
+A=C(input().split())
+for _ in "x"*75:
  N=C()
  for n,c in A.items():
   l=len(n)
   if n=="0":N["1"]+=c
-  elif l%2<1:m=l//2;N[n[:m]]+=c;N[str(int(n[m:]))]+=c
-  else:N[str(int(n)*2024)]+=c
+  elif l%2:N[str(int(n)*2024)]+=c
+  else:l>>=1;N[n[:l]]+=c;N[str(int(n[l:]))]+=c
  A=N
 print(A.total())
